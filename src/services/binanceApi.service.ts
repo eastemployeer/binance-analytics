1cba2e537eb24811810e22238a4d47b4b03f4ce6
eastemployeer <kamilzab27@gmail.com>
2026-01-22 12:01:09 +0100

    feat: added binanceApi service

---
+++ src/services/binanceApi.service.ts
@@ -0,0 +1,19 @@
+import { GetSymbolDataAnalysisRequest } from "@src/types/api/market";
+import { MarketCandle } from "@src/types/market";
+import Binance from 'binance-api-node';
+
+const client = Binance();
+
+export const fetchCandles = async (data: GetSymbolDataAnalysisRequest): Promise<MarketCandle[]> => {
+
+    const response = await client.candles(data);
+
+    const candles = response.map((candle) => ({
+        openTime: new Date(candle.openTime),
+        open: parseFloat(candle.open),
+        close: parseFloat(candle.close),
+        closeTime: new Date(candle.closeTime)
+    }))
+
+    return candles;
+}
