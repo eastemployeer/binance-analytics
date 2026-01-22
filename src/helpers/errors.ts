432db1bbedc87e4573cbe4d9bde51f3e472bd22e
eastemployeer <kamilzab27@gmail.com>
2026-01-22 12:01:37 +0100

    feat: extended ErrorCode and errorMessages

--- src/helpers/errors.ts
+++ src/helpers/errors.ts
@@ -1,7 +1,9 @@
 export enum ErrorCode {
-    BINANCE_API_ERROR = 1000
+    BINANCE_API_ERROR = 1000,
+    VALIDATION_ERROR = 1001,
 }
 
 export const errorMessages = {
-    [ErrorCode.BINANCE_API_ERROR]: 'Binance API Error'
+    [ErrorCode.BINANCE_API_ERROR]: 'Binance API Error',
+    [ErrorCode.VALIDATION_ERROR]: 'Validation Error'
 }
